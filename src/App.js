bf7b7fd82f1d4215e197364a892dbac662150657
Lucas Kauer <lucasgkauer@gmail.com>
2021-11-19 17:30:58 -0300

    barra app

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import AdminPrivateRoute from './AdminPrivateRoute';
 import axios from 'axios';
 
 // axios.defaults.baseURL = 'http://localhost:8000';
-axios.defaults.baseURL = 'https://plantiverso-backend-2ylj3.ondigitalocean.app'
+axios.defaults.baseURL = 'https://plantiverso-backend-2ylj3.ondigitalocean.app/app'
 
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers.post['Accept'] = 'application/json';
