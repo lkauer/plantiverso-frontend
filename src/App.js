7488d5ee84e66f95ebc56435eb223a52e98f1824
Lucas Kauer <lucasgkauer@gmail.com>
2021-11-19 17:23:38 -0300

    url

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Register from './components/frontend/auth/Register';
 import AdminPrivateRoute from './AdminPrivateRoute';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:8000';
+// axios.defaults.baseURL = 'http://localhost:8000';
+axios.defaults.baseURL = 'https://plantiverso-backend-2ylj3.ondigitalocean.app'
 
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers.post['Accept'] = 'application/json';
